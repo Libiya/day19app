df6a9a75d4e155a03d54069e614015c220559b79
Libiya <libiyapathrose99@gmail.com>
2021-11-09 21:16:34 +0530

    day19app

---
+++ src/customer-app.js/custtable.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './mytable.css';
+import custform from './customer-app/custform'
+const TableHeader = () => {
+    return (<thead className="table-header" >
+        <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Remove</th>
+            </tr>
+            </thead>
+            )}
+            const TableBody = (props) => {
+                const rows = props.characterData.map((row, index) =>
+                 {
+                     return (
+                     <tr key={index}>
+                         <td>{row.name}</td>
+                         <td>{row.city}</td>
+                         <td>{row.phone}</td>
+                         <td>{row.email}</td>
+                         
+                         <td><button className="button-del" onClick={()=>props.removeCharacter(index)}>Delete</button>
+                         </td>
+                         </tr>
+                         )}
+                         )
+                         return 
+                         <tbody>
+                             {rows}
+</tbody>}
+const Table= (props)=>
+ {
+     const
+    {characterData,removeCharacter} =propsreturn
+ (
+ <table>
+     <TableHeader />
+     <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+     </table>
+     )
+    
+    }
+    class StateData extends React.Component {state={characters:[],}removeCharacter = (index) => 
+    {const {characters} = this.state
+    /*filter does not mutate but rather creates a new array,and is a preferred method for modifying arrays in JavaScript. */
+this.setState({characters: characters.filter((character, i) => {
+    return i !== index}),
+})
+}
+    }
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    };
+    render() 
+    {
+        const {characters} =this.statereturn (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={
+                this.removeCharacter} />
+            <Form handleSubmit={this.handleSubmit}/>
+            </div>)}
+    export default StateData;
---
+++ src/game/square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './mygame.css'
+function Square(props) 
+{return 
+    (
+    <button className="square" onClick={props.onClick}>{props.value}</button>);
+}
